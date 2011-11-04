3117c27e62dbbaf1e9b3eaa08b790d234df91404
Michael Stämpfli <michael.staempfli@garaio.com>
2011-11-04 15:56:09 +0100

    added library to manipulate a docx

--- lib/docx_creator.rb
+++ lib/docx_creator.rb
@@ -1,5 +1,22 @@
-require "docx_creator/version"
+require 'docx_creator/docx_manipulator'
+require 'docx_creator/version'
+require 'nokogiri'
 
 module DocxCreator
-  # Your code goes here...
+
+  class << self
+    def translate(template, xslt, data, result_file)
+      f = DocxManipulator.new(template, result_file)
+      f.content = transform_data(xslt, data)
+      f.process
+    end
+
+    def transform_data(xslt_file, data_file)
+      xslt = Nokogiri::XSLT.parse(File.open(xslt_file))
+      data = Nokogiri::XML.parse(File.open(data_file))
+      xslt.transform(data).to_s
+    end
+    private :transform_data
+  end
+
 end
---
+++ spec/docx_creator/docx_manipulator_spec.rb
@@ -0,0 +1,33 @@
+require 'docx_creator/docx_manipulator'
+require 'zip/zip'
+
+describe DocxCreator::DocxManipulator do
+
+  subject { DocxCreator::DocxManipulator.new('spec/files/movies.docx', 'spec/files/result.docx') }
+
+  describe ".source_content" do
+    it "returns the content of document.xml" do
+      subject.source_content.should =~ /w:document/
+    end
+  end
+
+  describe ".process" do
+    after :each do
+      File.delete 'spec/files/result.docx'
+    end
+
+    it "generates the resulting document" do
+      subject.process
+      File.should exist('spec/files/result.docx')
+    end
+
+    it "replaces the content of the document" do
+      subject.content = 'bla'
+      subject.process
+      Zip::ZipFile.open('spec/files/result.docx') do |file|
+        file.get_input_stream('word/document.xml').read.should == 'bla'
+      end
+    end
+  end
+
+end
