25fa5a6f7a96125ca00cd6c865b69a5f4ac208aa
Robert Schaefer <rs@oxon.ch>
2013-06-17 12:28:42 +0200

    added wrapper to require all matchers

--- lib/docx_manipulator/matchers.rb
+++ lib/docx_manipulator/matchers.rb
@@ -1,1 +1,4 @@
+require 'docx_manipulator/matchers/be_a_docx'
 require 'docx_manipulator/matchers/be_like_docx'
+require 'docx_manipulator/matchers/contain_docx_file'
+require 'docx_manipulator/matchers/contain_docx_text'
