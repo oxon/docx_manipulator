27e76deb646c88e97a0fa12664626d9d7813dfa1
Noel Rohrbach <nr@oxon.ch>
2014-10-01 11:58:30 +0200

    Fix contain_docx_file matcher
    Zip::File.foreach returns no Enumerator anymore, it calls the block

--- lib/docx_manipulator/matchers/contain_docx_file.rb
+++ lib/docx_manipulator/matchers/contain_docx_file.rb
@@ -2,7 +2,11 @@ RSpec::Matchers.define :contain_docx_file do |file|
   match do |actual|
     content = file.read
     Zip::File.open(actual) do |sample_file|
-      Zip::File.foreach(actual).any? { |f| sample_file.read(f) == content }
+      contains = []
+      Zip::File.foreach(actual) do |f|
+        contains << (sample_file.read(f) == content)
+      end
+      contains.any?
     end
   end
 end
